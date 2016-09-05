f8b26b4a356b3beb229e09479831f7fdc2abba8b
l <aa@google.com>
2016-09-05 23:52:00 +0900

    change2

--- QUADTREE.java
+++ QUADTREE.java
@@ -1,13 +1,18 @@
 import java.util.*;
-
 /*
-  * test case
-3
+4
+w
 xbwwb
 xbwxwbbwb
 xxwwwbxwxwbbbwwxxxwwbbbwwwwbb
+
+w
+xwbbw
+xxbwwbbbw
+xxwbxwwxbbwwbwbxwbwwxwwwxbbwb
  */
-public class QUADTREE {
+
+class QUADTREE {
 
 	static int t,n;
 	//static String str;
@@ -15,29 +20,24 @@ public class QUADTREE {
 		
 		Scanner sc = new Scanner(System.in);
 		t = sc.nextInt();
-		
+		sc.nextLine();
 		for(int test_case=0; test_case< t; test_case++){
-			String str = sc.next();
-			String result = reverse(str);
-			System.out.println(" #result: " + result);
+			String str = sc.nextLine();
+			System.out.println( reverse(str));
 		}
 	}
 	
 	static String reverse(String pstr){
+		String btm_left = new String() ;
+		String btm_right = new String() ;
+		String upp_left = new String();
+		String upp_right = new String();
 		
-		String head = pstr.charAt(0)+"";
-		
-		//System.out.println(" #pstr: " + pstr);
-		//System.out.println(" #head: " + head);
-		String btm_left ="",btm_right="",upp_left="", upp_right="";
-		
-		if("b".equals(head) || "w".equals(head)){
-			return head;
-		}
-		
-		
-		if("x".equals(head)){
+		if('x' != pstr.charAt(0) ){
+			return pstr.charAt(0)+"";
+		}else{
 			int begin_idx =1;
+			
 			upp_left =reverse(pstr.substring(begin_idx));
 			begin_idx +=upp_left.length();
 			
@@ -48,7 +48,9 @@ public class QUADTREE {
 			begin_idx +=btm_left.length();
 			
 			btm_right =reverse(pstr.substring(begin_idx));
+			
 		}
+		
 		return "x"+btm_left+btm_right+upp_left+upp_right;
 	}
 
